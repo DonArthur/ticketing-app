9ffc9a8f7dc2532005fe64a0ee413ea6b3a1ac5a
DonArthur <geraldy.miero@gmail.com>
2023-11-05 07:46:35 +0700

    update: dashboard styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,14 @@ type Props = {}
 
 const Dashboard = (props: Props) => {
   return (
-    <div><TicketCard /></div>
+    <div className="p-5">
+      <div className="lg:grid lg:grid-cols-2 xl:grid xl:grid-cols-4">
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
+    </div>
   )
 }
 
